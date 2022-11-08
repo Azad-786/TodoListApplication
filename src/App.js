bea83abb79b5ccf64836cecb6b911bd824517ca3
Azad-786 <azadvocalss@gmail.com>
2022-11-08 22:04:51 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Styyle.css";
+import TodoList from "./components/TodoAppList";
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
